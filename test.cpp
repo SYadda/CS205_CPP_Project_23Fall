4dae9d73318b27070afb2f883b9c227bf122fc84
Delta0019 <125425538+Delta0019@users.noreply.github.com>
2024-01-20 10:21:17 +0800

    Update test.cpp
    更改为按照顺序进行测试，并使得测试更加详细。

--- test.cpp
+++ test.cpp
@@ -26,6 +26,21 @@ int main()
     cout << t2 << endl
          << endl;
 
+    // 测试index_select
+    cout << endl
+         << "--------------------测试index_select----------------------" << endl
+         << endl;
+    vector<int> index = {0};
+    cout << "---index = {0}---" << endl
+         << endl;
+    ts::Tensor<float> t10 = t1(index);
+    cout << t10 << endl
+         << endl;
+    cout << "---index = {0}, range = {0, 2}---" << endl
+         << endl;
+    ts::Tensor<float> t5 = t1(index, {0, 2});
+    cout << t5 << endl;
+
     // 测试join
     cout << endl
          << "--------------------测试join----------------------" << endl
@@ -41,13 +56,39 @@ int main()
     ts::Tensor<float> t4 = tile(t2, reps);
     cout << t4 << endl;
 
-    // 测试index_select
+    // 测试mutating1
     cout << endl
-         << "--------------------测试index_select----------------------" << endl
+         << "--------------------测试mutating1----------------------" << endl
          << endl;
-    vector<int> index = {0};
-    ts::Tensor<float> t5 = t1(index, {0, 2});
-    cout << t5 << endl;
+    vector<int> goal;
+    vector<float> data = {1};
+    goal.push_back(1);
+    t2(goal) = data;
+    cout << t2 << endl;
+
+    // 测试mutating2
+    cout << endl
+         << "--------------------测试mutating2----------------------" << endl
+         << endl;
+    vector<int> goal2 = {0, 1};
+    vector<float> data_2 = {100, 200};
+    pair<int, int> range = {0, 2};
+    t2(goal2, range) = data_2;
+    cout << t2 << endl;
+
+    // 测试transpose
+    cout << endl
+         << "--------------------测试transpose----------------------" << endl
+         << endl;
+    cout << "---将t1的第1维和第2维交换---" << endl
+         << endl;
+    ts::Tensor<float> t8 = t1.tensor_transpose(2, 1);
+    cout << t8 << endl
+         << endl;
+    cout << "---将t1复原---" << endl;
+    ts::Tensor<float> t9 = t8.tensor_transpose(2, 1);
+    cout << endl
+         << t9 << endl;
 
     // 测试permute
     cout << endl
@@ -61,8 +102,8 @@ int main()
     cout << endl
          << "--------------------测试view----------------------" << endl
          << endl;
-    vector<int> size = {2, 12};
-    ts::Tensor<float> t7 = t1.view(size);
+    vector<int> size = {1, 2, 2, 4};
+    ts::Tensor<float> t7 = t2.view(size);
     cout << t7 << endl;
 
     return 0;
